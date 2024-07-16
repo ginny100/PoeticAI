06e422fc5315f5fcb0a24f747002fa10bedc0170
Ginny-Dang <ginnydang0728@gmail.com>
2024-07-16 05:35:49 -0500

    added seq2seq class template

---
+++ model/seq2seq/model.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+class Seq2Seq(tf.keras.Model):
+    def __init__(self):
+        super(Seq2Seq, self).__init__()
+        
+    
+    def call(self, inputs):
+        pass
