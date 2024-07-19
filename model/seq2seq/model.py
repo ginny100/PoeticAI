52bb8e2b6438199198c1ccfbafbe45886d28df78
Ginny-Dang <ginnydang0728@gmail.com>
2024-07-19 01:42:48 -0500

    added seq2seq model

--- model/seq2seq/model.py
+++ model/seq2seq/model.py
@@ -10,3 +10,26 @@ class Seq2Seq(tf.Keras.Model):
         self.decoder = Decoder(output_text_processor.vocabulary_size(), embedding_dim, units)
         self.input_text_processor = input_text_processor
         self.output_text_processor = output_text_processor
+    
+    @tf.function
+    def train_step(self, data):
+        input_sentence, output_sentence = data
+        input_word_indices = self.input_text_processor(input_sentence)
+        output_word_indices = self.output_text_processor(output_sentence)
+        output_mask = tf.cast(output_word_indices!=0, tf.float32)
+        
+        with tf.GradientTape() as tape:
+            whole_encoder_states, final_hidden_state = self.encoder(input_word_indices)
+            vocab_output, decoder_last_state = self.decoder(
+                input_word_indices=output_word_indices,
+                encoder_keys=whole_encoder_states,
+                mask=(input_word_indices != 0),
+                state=final_hidden_state
+            )
+        
+        loss = tf.reduce_sum(self.loss(y_true=output_word_indices[:, 1:], y_pred=vocab_output[:, :-1])*output_mask[:, 1:])
+        variables = self.trainable_variables
+        gradients = tape.gradient(loss, variables)
+        self.optimizer.apply_gradients(zip(gradients, variables))
+        
+        return self._step(data, training=True)
