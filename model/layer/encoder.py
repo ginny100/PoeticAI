92f785a84466ad650b2942aa847e5d6818eb4b6d
Ginny-Dang <ginnydang0728@gmail.com>
2024-08-13 07:15:30 -0500

    fixed encoder

--- model/layer/encoder.py
+++ model/layer/encoder.py
@@ -3,15 +3,20 @@ import tensorflow as tf
 from tensorflow.keras.layers import Layer, Embedding, LSTM # type: ignore
 
 class Encoder(Layer):
-    def __init__(self, encoder_input, input_vocab_size, latent_dim):
+    def __init__(self, input_vocab_size, latent_dim):
         super(Encoder, self).__init__()
-        self.encoder_emb = Embedding(input_vocab_size, latent_dim, trainable=True)(encoder_input) # Encoder
-        self.encoder_lstm1 = LSTM(latent_dim,return_sequences=True,return_state=True) # LSTM 1 
-        self.encoder_lstm2 = LSTM(latent_dim,return_sequences=True,return_state=True) # LSTM 2 
-        self.encoder_lstm3 = LSTM(latent_dim, return_state=True, return_sequences=True) # LSTM 3
+        print("Encoder - latent_dim", latent_dim)
+        self.encoder_emb = Embedding(input_vocab_size, latent_dim, trainable=True) # Encoder
+        self.encoder_lstm1 = LSTM(latent_dim, return_sequences=True, return_state=True) # LSTM 1 
+        self.encoder_lstm2 = LSTM(latent_dim, return_sequences=True, return_state=True) # LSTM 2 
+        self.encoder_lstm3 = LSTM(latent_dim, return_sequences=True, return_state=True) # LSTM 3
 
-    def call(self):
-        encoder_output1, _, _ = self.encoder_lstm1(self.encoder_emb)
+    @tf.function
+    def call(self, inputs):
+        encoder_emb = self.encoder_emb(inputs)
+        encoder_output1, _, _ = self.encoder_lstm1(encoder_emb)
         encoder_output2, _, _ = self.encoder_lstm2(encoder_output1)
         encoder_outputs, state_h, state_c = self.encoder_lstm3(encoder_output2) 
+        print("Encoder - state_h", state_h)
+        print("Encoder - state_c", state_c)
         return encoder_outputs, state_h, state_c
